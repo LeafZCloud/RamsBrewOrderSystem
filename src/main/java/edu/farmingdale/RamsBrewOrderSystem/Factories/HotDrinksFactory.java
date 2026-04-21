9da0dce5208858d952568bdf55764dfdefbdd36e
Brandon Douglass <brandonjdouglass@yahoo.com>
2026-04-21 12:59:13 -0400

    Completed Abstract Factory implementation for hot and iced drinks and implemented DrinkBuilder with switch-case for customization options

--- src/main/java/edu/farmingdale/RamsBrewOrderSystem/Factories/HotDrinksFactory.java
+++ src/main/java/edu/farmingdale/RamsBrewOrderSystem/Factories/HotDrinksFactory.java
@@ -1,4 +1,26 @@
 package edu.farmingdale.RamsBrewOrderSystem.Factories;
 
-public class HotDrinksFactory {
+import edu.farmingdale.RamsBrewOrderSystem.Drinks.*;
+
+public class HotDrinksFactory implements DrinksFactory {
+
+    @Override
+    public Drinks createCoffee() {
+        return new Coffee("Hot");
+    }
+
+    @Override
+    public Drinks createLatte() {
+        return new Latte("Hot");
+    }
+
+    @Override
+    public Drinks createCappuccino() {
+        return new Cappuccino("Hot");
+    }
+
+    @Override
+    public Drinks createTea() {
+        return new Tea("Hot");
+    }
 }
